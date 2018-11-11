9ff421a9518f4e035a4840e0e4f7f67cc8cccb85
Nick Heidinger <playnich8@hotmail.com>
2018-11-10 23:36:26 -0600

    client script update

--- Client/client_init.py
+++ Client/client_init.py
@@ -22,27 +22,34 @@ def init_log():
 	#initilizes log file
 	date = datetime.date.today().strftime("%Y-%b-%dT%I_%m_%S")
 	global log_name
-	log_name = "client_secureCam_log_" + date
+	log_name = "client_secureCam_log_" + date + ".txt"
 	log_file = open(log_name,'w') #creates log
 
 def write_to_log(log):
 	#writes string to log
 	print log
-	log_file = open(log_name, 'w')
+	log_file = open(log_name, 'a')
 	log_file.write(log)
 	log_file.close()
 
-def image_capture():
+def image_capture(cam):
 	#Captures image
-	cam = VideoCapture(0)
 	s,img = cam.read()
 	if s:
-		namedWindow("cam-test")
 		imwrite("filename.jpg",img)
+		return img
+	return False
 
-def image_processing():
+def image_motion_detect(img1,img2):
 	#Processes image
-	print "TODO"
+	frameDelta = cv2.absdiff(img1, img2)
+	thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+
+	# dilate the thresholded image to fill in holes, then find contours
+	# on thresholded image
+	thresh = cv2.dilate(thresh, None, iterations=2)
+
+
 
 def server_connect():
 	#Get server credentials and connect
@@ -64,13 +71,30 @@ def main():
 	#main
 	init_log()
 	write_to_log("Client Starting...")
+	#server_connect()
+
+	#Get initial frames
+	cam = VideoCapture(0)
+	prev_image = image_capture(cam)
+	time.sleep(1)
+	curr_image = image_capture(cam)
+	image_motion_detect(prev_image,curr_image)
+	#compare prev and curr for motion detection
 	
+	
+	#while no detection
+		#get new image and delay a few seconds
+		#if motion detection save previous and current and start video recording for a set time
+		#after done recording start cycle again and wait for motion
+		#when motion occurs save it to
+	#while():
 
-	server_connect()
-	image_capture()
+	#	prev_image = curr_image
+	#	curr_image = image_capture()
 
 	
-
+	write_to_log("Disconnecting Camera.")
+	cam.release()
 	write_to_log("End of Processing")
 
 
